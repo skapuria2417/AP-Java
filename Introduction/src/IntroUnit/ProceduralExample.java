4ef2a8fb785e569c0c55b92a63775a765611abb9
Student6 <Student6@10.8.33.172>
2016-09-15 12:59:36 -0400

    added hello world

--- Introduction/src/IntroUnit/ProceduralExample.java
+++ Introduction/src/IntroUnit/ProceduralExample.java
@@ -8,8 +8,8 @@ package IntroUnit;
  *:) jksvklnklvnvhnl  
  */
 public class ProceduralExample {
+	public static void main(String []args){
+		  System.out.println("Hello World");
+		}
+}
 
-}
-public static void main(String []args){
-  System.out.println("Hello World");
-}
