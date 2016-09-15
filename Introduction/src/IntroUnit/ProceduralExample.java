73554d998c51019cf912ea910020dd7a3ead239a
skapuria2417 <skapuria2417@bths.edu>
2016-09-15 13:19:22 -0400

    year

--- Introduction/src/IntroUnit/ProceduralExample.java
+++ Introduction/src/IntroUnit/ProceduralExample.java
@@ -12,6 +12,8 @@ public class ProceduralExample {
 		System.out.println("Hello World");
 		String[] names = {"Jillian", "Joseph", "Jordan"};
 		System.out.println("My Name is " + names[0]+".");
+		String [] year ={"senior", "junior", "sophomore","freshmen"};
+		System.out.println{"I am a " + year[0] + "."};
 	}
 }
 
