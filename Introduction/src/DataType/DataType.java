1944abab1bc501708cac7fdc2bf20eb78028dd76
Student6 <Student6@10.8.33.172>
2016-09-19 13:15:54 -0400

    data types

---
+++ Introduction/src/DataType/DataType.java
@@ -0,0 +1,18 @@
+package DataType;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		System.out.println(5.0/2);//double divided by int
+		System.out.println(5/2);//int divided by int
+		System.out.println((double) 5/2);
+		System.out.println( 3+5.0/2+5*2);
+		System.out.println(  3.0+5/2+5*2);
+		System.out.println( (int)(3.0 + 5)/(2 + 5 * 2));
+		System.out.println( 3.0+5/2+5*2);
+		
+		if(42 == 42.0) System.out.println("42 equals 42.0");
+
+	}
+
+}
--- Introduction/src/IntroUnit/Junior.java
+++ Introduction/src/IntroUnit/Junior.java
@@ -6,6 +6,7 @@ public class Junior extends Student {
 	public Junior(String name, String majors) {
 		super(name);
 		this.majors = majors;
+		
 	}
 	
 	public void talk(){
