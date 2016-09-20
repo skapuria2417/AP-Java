3234e6f528856fcb51e66a79699fa51efe7b9301
Student6 <Student6@10.8.33.172>
2016-09-20 12:34:02 -0400

    dice

--- Introduction/src/DataType/DataType.java
+++ Introduction/src/DataType/DataType.java
@@ -3,15 +3,40 @@ package DataType;
 public class DataType {
 
 	public static void main(String[] args) {
-		System.out.println(5.0/2);//double divided by int
+		/*System.out.println(5.0/2);//double divided by int
 		System.out.println(5/2);//int divided by int
 		System.out.println((double) 5/2);
 		System.out.println( 3+5.0/2+5*2);
 		System.out.println(  3.0+5/2+5*2);
 		System.out.println( (int)(3.0 + 5)/(2 + 5 * 2));
 		System.out.println( 3.0+5/2+5*2);
+		System.out.println(.1+.2);
 		
-		if(42 == 42.0) System.out.println("42 equals 42.0");
+		if(42==42.0) System.out.println("it's equal");
+		if(.1+.2==0.30000000000000004) System.out.println("it's equal");*/
+		
+//		String s = ""+1;
+//		System.out.println(s);
+		
+		/*String s = "Happy "+18+"th birthday!";
+		System.out.println(s);
+		
+		String ten = "10";
+		String two = "2";
+		
+		if (ten.compareTo(two)>0) System.out.println("10 is greater than two");
+		else System.out.println("10 is not greater than two");
+		
+		String q = "Happy 18th birthday!";
+		System.out.println(q);*/
+		
+		int m = 22;
+		int n = 7;
+		System.out.println("m/n is "+(m/n));
+		
+		int l = 22;
+		int q = 7;
+		System.out.println("m/n is "+((double)l/q));
 
 	}
 
---
+++ Introduction/src/MathRandom.java
@@ -0,0 +1,14 @@
+
+import java.util.Random;
+public class MathRandom {
+
+	public static void main(String[] args) {
+		for (int index = 0; index < 10; index ++){
+			System.out.println("Roll #"+ (index+1) + " Die is " + rollFairDie());
+		}
+	}
+	public static int rollFairDie(){
+		Random gen = new Random();
+		return gen.nextInt(6)+1;
+	}
+}
