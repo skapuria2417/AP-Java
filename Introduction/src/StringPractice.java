d4ebf12a7ee98367f579d9f2577e33f80b08bc7b
Student6 <Student6@10.8.33.172>
2016-09-21 13:17:30 -0400

    wasaasdasdasd

--- Introduction/src/StringPractice.java
+++ Introduction/src/StringPractice.java
@@ -3,8 +3,20 @@ public class StringPractice {
 
 	public static void main(String[] args) {
 	//	String text = new String ("Hello World");
-		String text = "Hello World";
-		System.out.println(text);
+		String text1 = "Hello World";
+		String text2 = "Hello ";
+		String text3 = "World";
+		if (text1.equals( text2 + text3)){
+			System.out.println("These String are Equal.");
+		}
+		System.out.println(text1);
+		System.out.println(text2 + text3);
+		
+		String word1 = "Aardvark";
+		String word2 = "Zzzybanaa";
+		if (word1.compareTo(word2)<0){
+			System.out.println("These String are Equal.");
+		}
 	}
 
 }
