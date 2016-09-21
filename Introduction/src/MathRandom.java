4c18e75530faf39c797ae1ccfb4c73258f899436
Student6 <Student6@10.8.33.172>
2016-09-21 13:02:43 -0400

    dice finished string practice

--- Introduction/src/MathRandom.java
+++ Introduction/src/MathRandom.java
@@ -3,12 +3,31 @@ import java.util.Random;
 public class MathRandom {
 
 	public static void main(String[] args) {
-		for (int index = 0; index < 10; index ++){
-			System.out.println("Roll #"+ (index+1) + " Die is " + rollFairDie());
+		int[] results = new int[6];
+		//same as
+		//int[] = [0,0,0,0,0,0]
+		int  totalRolls = 10000;
+
+		for (int index = 0; index < 10000; index ++){
+			int roll = rollUnfairDie();
+			System.out.println("Roll #"+ (index+1) + " Die is " +roll );
+			results[roll-1]++;
+		}
+		for(int i = 0; i < 6; i ++){
+			System.out.println((i+1)+" was rolled "+ results[i] + " times"+ ". And the percentage is " +(double) (((double)results[i]/totalRolls *1000)/10.0) + "%."  );
 		}
 	}
 	public static int rollFairDie(){
-		Random gen = new Random();
-		return gen.nextInt(6)+1;
+		//		Random gen = new Random();
+		//		return gen.nextInt(6)+1;
+
+		double rand = Math.random();//creates random double (0,1)
+		int roll = (int) (6 * rand);//[0,5](this is what above code does)
+		return roll+1;
+	}
+	public static int rollUnfairDie(){
+		int[] myList = {6,6,6,6,6,6,6,6,6,6,6,2,2,4,4,3,3,5,5,5,5,1,2};
+		int rnd = new Random().nextInt(myList.length);
+		return myList[rnd];
 	}
 }
---
+++ Introduction/src/StringPractice.java
@@ -0,0 +1,10 @@
+
+public class StringPractice {
+
+	public static void main(String[] args) {
+	//	String text = new String ("Hello World");
+		String text = "Hello World";
+		System.out.println(text);
+	}
+
+}
