c47889c7fecda22c2b8784adee3d55baff94168a
Student6 <Student6@10.8.33.172>
2016-09-26 12:34:40 -0400

    qweewq

--- Introduction/src/ChatBot/SammyajitMain.java
+++ Introduction/src/ChatBot/SammyajitMain.java
@@ -3,18 +3,18 @@ package ChatBot;
 import java.util.Scanner;
 
 public class SammyajitMain {
-	
+
 	static Scanner input;
 	static String user;
-	static int lineCount;
+	//static int lineCount;
 	static boolean inLoop;
 	static String response;
 	static Topic school;
-	
-	
+
+
 	public static void main(String[] args) {
 		createTopics();
-		lineCount = 0;
+		//lineCount = 0;
 		//demonstrateStringMethod();
 		promptName();
 		talkForever();
@@ -26,7 +26,7 @@ public class SammyajitMain {
 		user=input.nextLine();
 		print("Awesome I will call you "+ user + ". Until you terminate me.");
 	}
-	
+
 	public static String getInput(){
 		return input.nextLine();
 	}
@@ -37,39 +37,61 @@ public class SammyajitMain {
 		String userInput = input.nextLine();
 		print("You typed " + userInput);
 	}
-	
+
 	public static void talkForever(){
 		inLoop = true;
 		while(inLoop){
 			print("Greetings, "+ user +". How are you?");
 			response = getInput();
-			if (response.indexOf("good") >= 0){
+			if (findKeyword(response, "good",0) ){
 				print("I am so happy to here that.");
 			}
 			else{
 				if(response.indexOf("school")>= 0){
 					inLoop = false;//exit this loop
 					school.talk();
-					
+
 				}else{
 					print("I'm sorry I do not understand.");
 				}
-						
+
 			}
-			
-			
-			
+
+
+
 		}
-		
+
 	}
+	public static boolean findKeyword(String searchString, String key, int startIndex) {
+		String phrase = searchString.trim();
+		//set all the letters to lowercase
+		phrase = phrase.toLowerCase();
+		key = key.toLowerCase();
+		int psn = phrase.indexOf(key);
+		//keep looking for the word until you find the right context.
+		while(psn >= 0){
+			String before = " ";
+			String after = " ";
+			if(psn + key.length()< phrase.length()){
+				after = phrase.substring(psn + key.length(), psn + key.length() + 1).toLowerCase();
+			}
+			if (before.compareTo("a") < 0 && after.compareTo("a") < 0){
+				return true;
+			}
+			psn = phrase.indexOf(key,psn + 1);
+		}
+		return false;
+	}
+
+
 	public static void print(String s) {
 		String printString = "";
 		int cutoff = 35;
 		while (s.length()>0){
 			String currentLine = "";
 			String nextWord = "";
-				//while the currentLine and nextWord are less 
-				//than the cutoff. AND there are still words to add do the following loop
+			//while the currentLine and nextWord are less 
+			//than the cutoff. AND there are still words to add do the following loop
 			while (currentLine.length() + nextWord.length() <= cutoff && s.length() > 0){
 				//add the next word to the line
 				currentLine += nextWord;
@@ -86,11 +108,12 @@ public class SammyajitMain {
 			}
 			printString += currentLine + "\n";
 		}
-		
+
 		System.out.println(printString);
 	}
 
 	public static void createTopics() {
 		input = new Scanner(System.in);
+		school =new school();
 	}
 }
--- Introduction/src/ChatBot/School.java
+++ Introduction/src/ChatBot/School.java
@@ -4,10 +4,18 @@ public class School implements Topic {
 	private boolean inSchoolLoop;
 	private String schoolResponse;
 	public void talk(){
+		inSchoolLoop=true;
 		while (inSchoolLoop){
+			//inSchoolLoop = true;
 			SammyajitMain.print("tell me about school.");
 			schoolResponse = SammyajitMain.getInput();
-			SammyajitMain.print("Thats my favorite "+ "part about school too.");
+			//SammyajitMain.print("Thats my favorite "+ "part about school too.");
+			if (schoolResponse.indexOf("stop")>=0){
+				inSchoolLoop = false;
+				SammyajitMain.talkForever();
+			}else{
+				SammyajitMain.print("Thats my favorite part about school too.");
+			}
 		}
 	}
 
