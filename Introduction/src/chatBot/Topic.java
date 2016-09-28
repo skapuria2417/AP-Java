ff410e63d980b35049e88fb31a4af5167d920ad1
Student6 <Student6@10.8.33.172>
2016-09-28 12:07:58 -0400

    9/28

--- Introduction/src/chatBot/Topic.java
+++ Introduction/src/chatBot/Topic.java
@@ -2,4 +2,16 @@ package chatBot;
 
 public interface Topic {
 	public void talk();
+	public boolean isTriggered(String userInput);
+		
 }
+/*
+if (SammyajitMain.findKeyword(userInput, "school", 0)>= 0){
+			return true;
+		}
+	if (SammyajitMain.findKeyword(userInput, "class", 0)>= 0){
+		return true;
+	}
+	return false;
+	}
+*/
