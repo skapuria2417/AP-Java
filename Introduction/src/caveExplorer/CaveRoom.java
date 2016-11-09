afa00a5c3b38c038e54bbd7c3aae096ea502376e
Student6 <Student6@10.8.33.172>
2016-11-09 12:34:10 -0500

    asdasdsdsa

--- Introduction/src/caveExplorer/CaveRoom.java
+++ Introduction/src/caveExplorer/CaveRoom.java
@@ -18,7 +18,7 @@ public class CaveRoom {
 
 	public CaveRoom(String description){
 		this.description = description;
-		setDefaultContents("   ");
+		setDefaultContents(" ");
 		contents = defaultContents;
 		
 		borderingRooms = new CaveRoom[4];
@@ -47,12 +47,17 @@ public class CaveRoom {
 	
 	}
 
+	public static String toDirection(int dir) {
+		String[] directions = {"the North","the East","the South","the West"};
+		return directions[dir];
+	}
+
 	public String getContents(){
 		return contents;
 	}
 	
 	public void enter(){
-		contents = " X ";
+		contents = "X";
 	}
 	
 	public void leave(){
@@ -109,7 +114,34 @@ public class CaveRoom {
 	}
 
 	public void interpretinput(String input) {
-		// TODO Auto-generated method stub	
+		while(!isValid(input)){
+			System.out.println("You can only input 'w', 'a','s','d.'");
+			input = CaveExplorer.in.nextLine();
+		}
+		String[] keys = {"w","a","s","d"};
+		int indexFound = -1;
+		for(int i =0; i< keys.length;i++){
+			if(input.equals(keys[i])){
+				indexFound = i;
+				break;
+			}
+		}
+		if(borderingRooms[indexFound]!= null && doors[indexFound].isOpen()){
+			CaveExplorer.currentRoom.leave();
+			CaveExplorer.currentRoom=borderingRooms[indexFound];
+			CaveExplorer.currentRoom.enter();
+			CaveExplorer.inventory.updateMap();
+		}
+	}
+
+	private boolean isValid(String s) {
+		String[] keys = {"w","a","s","d"};
+		for(int i = 0; i < keys.length; i++){
+			if(s.equals(keys[i])){
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
--- Introduction/src/caveExplorer/Inventory.java
+++ Introduction/src/caveExplorer/Inventory.java
@@ -10,10 +10,41 @@ public class Inventory {
 	}
 	
 	
-	private void updateMap() {
-		// TODO Auto-generated method stub
+	public void updateMap() {
+		map= " ";
+		for(int i = 0; i < CaveExplorer.caves[0].length-1;i++){
+			map+="____";
+			
+		}
+		map+="___\n";
 		
+		for(CaveRoom[] row: CaveExplorer.caves){
+			for(int i = 0;i<3;i++){
+				String text ="";
+				for(CaveRoom cr: row){
+					text+="|";
+					if(cr.getDoor(CaveRoom.WEST) != null && cr.getDoor(CaveRoom.WEST).isOpen()){
+						text =" ";
+					}
+					if(i==0){
+						text +="   ";
+					}else if(i==1){
+						text+=" "+ cr.getContents();
+					}else if (i==2){
+						if(cr.getDoor(CaveRoom.SOUTH)!= null && cr.getDoor(CaveRoom.SOUTH).isOpen()){
+							text+="   ";
+						}else{
+							text+="___";
+						}
+					}
+				}//last caveroom in the row.
+				text+="|";
+				map+= text + "\n";
+			}//last of the three text lines.
+			//last row.
+		}
 	}
+	
 
 
 	public String getDescription() {
